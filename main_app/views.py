ebd050338ba8e1c8bfebe794e071c9f4782bf536
aprin418 <aprin418@gmail.com>
2021-06-24 14:13:46 -0400

    added cat toys

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from . models import Cat
+from . models import Cat, CatToy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+
 #create your views here
 
 def about(request):
@@ -24,20 +26,43 @@ def cats_show(request, cat_id):
     data = {'cat': cat}
     return render(request, 'cats/show.html', data)
 
+def profile(request, username):
+    user = User.objects.get(username=username)
+    cats = Cat.objects.filter(user=user)
+    return render(request, 'profile.html', {'username': username, 'cats': cats})
+
 class CatCreate(CreateView):
-  model = Cat
-  fields = '__all__'
-  success_url = '/cats'
+    model = Cat
+    # Update the line below - add 'cattoys'
+    fields = ['name', 'breed', 'description', 'age', 'cattoys']
 
 class CatUpdate(UpdateView):
-  model = Cat
-  fields = ['name', 'breed', 'description', 'age']
-
-  def form_valid(self, form):
-    self.object = form.save(commit=False)
-    self.object.save()
-    return HttpResponseRedirect('/cats/' + str(self.object.pk))
+    model = Cat
+    # Update the line below - add 'cattoys'
+    fields = ['name', 'breed', 'description', 'age', 'cattoys']
     
 class CatDelete(DeleteView):
   model = Cat
   success_url = '/cats'
+
+def cattoys_index(request):
+    cattoys = CatToy.objects.all()
+    return render(request, 'cattoys/index.html', {'cattoys': cattoys})
+
+def cattoys_show(request, cattoy_id):
+    cattoy = CatToy.objects.get(id=cattoy_id)
+    return render(request, 'cattoys/show.html', {'cattoy': cattoy})
+
+class CatToyCreate(CreateView):
+    model = CatToy
+    fields = '__all__'
+    success_url = '/cattoys'
+
+class CatToyUpdate(UpdateView):
+    model = CatToy
+    fields = ['name', 'color']
+    success_url = '/cattoys'
+
+class CatToyDelete(DeleteView):
+    model = CatToy
+    success_url = '/cattoys'
