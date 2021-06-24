81ca38b30f37aef0e566624b650a6906e1574f6b
aprin418 <aprin418@gmail.com>
2021-06-24 16:24:56 -0400

    added auth

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . models import Cat, CatToy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 #create your views here
+
+def login_view(request):
+     # if post, then authenticate (user submitted username and password)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/user/'+u)
+                else:
+                    print('The account has been disabled.')
+        else:
+            print('The username and/or password is incorrect.')
+            return HttpResponseRedirect('/login')
+    else: # it was a get request so send the emtpy login form
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/cats')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/cats')
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
+
+
 
 def about(request):
     #3rd param with data will be added
@@ -26,6 +69,7 @@ def cats_show(request, cat_id):
     data = {'cat': cat}
     return render(request, 'cats/show.html', data)
 
+@login_required
 def profile(request, username):
     user = User.objects.get(username=username)
     cats = Cat.objects.filter(user=user)
@@ -33,14 +77,20 @@ def profile(request, username):
 
 class CatCreate(CreateView):
     model = Cat
-    # Update the line below - add 'cattoys'
-    fields = ['name', 'breed', 'description', 'age', 'cattoys']
+    fields = ['name', 'breed', 'description', 'age']
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect('/cats/' + str(self.object.pk))
 
 class CatUpdate(UpdateView):
     model = Cat
     # Update the line below - add 'cattoys'
     fields = ['name', 'breed', 'description', 'age', 'cattoys']
     
+@method_decorator(login_required, name='dispatch')
 class CatDelete(DeleteView):
   model = Cat
   success_url = '/cats'
